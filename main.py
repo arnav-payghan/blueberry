502bbdec24c314b1bb8dd39c0378391d4f5475f1
Arnav Payghan <arnav.payghan@gmail.com>
2025-01-15 01:50:14 +0530

    Day 2 changes - main.py Functions

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import eel
+
+eel.init('frontend')
+
+os.system('start msedge.exe --app="http://localhost:8080/index.html"')
+
+eel.start('index.html', port=8080, mode=None, host='localhost', block=True)
