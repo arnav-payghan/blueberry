fc3571a28a182b43464bfed3257e2aa73a7490bf
Arnav Payghan <arnav.payghan@gmail.com>
2025-02-05 17:48:05 +0530

    added contacts table and imported contacts.csv

--- engine/database.py
+++ engine/database.py
@@ -5,20 +5,27 @@ import sqlite3
 conn = sqlite3.connect("assistant.db")
 cursor = conn.cursor()
 
-# Creating a table sys command
-query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
-cursor.execute(query)
+### Creating a table SYS COMMAND
 
-# Keep Null as it is (AutoIncrementing), 'name' add name for calling, 'path' add path by double file location in PC.
-query = "INSERT INTO sys_command VALUES (null, 'discord', 'C:\\Users\\arnav\\AppData\\Local\\Discord\\Update.exe')"
-cursor.execute(query)
-conn.commit()
+#query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+#cursor.execute(query)
 
-# Create table : web_command
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-cursor.execute(query)
+### Keep Null as it is (AutoIncrementing), 'name' add name for calling, 'path' add path by double file location in PC.
 
-# Keep Null as it is (AutoIncrementing), 'name' add name for calling, 'url' add url by copying said url.
+#query = "INSERT INTO sys_command VALUES (null, 'discord', 'C:\\Users\\arnav\\AppData\\Local\\Discord\\Update.exe')"
+#cursor.execute(query)
+#conn.commit()
+
+### Create table : WEB COMMAND
+
+#query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+#cursor.execute(query)
+
+### Keep Null as it is (AutoIncrementing), 'name' add name for calling, 'url' add url by copying said url.
+
 #query = "INSERT INTO web_command VALUES (null, 'youtube', 'https://www.youtube.com/')"
 #cursor.execute(query)
 #conn.commit()
+
+# Creating a table with the desired columns
+cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
