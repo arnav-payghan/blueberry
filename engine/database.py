506717add385ad73b22ead4d8b69ea030d177130
Arnav Payghan <arnav.payghan@gmail.com>
2025-02-08 00:19:32 +0530

    whatsapp function (half done)

--- engine/database.py
+++ engine/database.py
@@ -1,11 +1,12 @@
 # ALL DATABASE CODES WILL BE ADDED HERE
 
+import csv
 import sqlite3
 
 conn = sqlite3.connect("assistant.db")
 cursor = conn.cursor()
 
-### Creating a table SYS COMMAND
+######################################## Creating a table SYS COMMAND ########################################
 
 #query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 #cursor.execute(query)
@@ -16,16 +17,48 @@ cursor = conn.cursor()
 #cursor.execute(query)
 #conn.commit()
 
-### Create table : WEB COMMAND
+######################################### Create table : WEB COMMAND #########################################
 
 #query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
 #cursor.execute(query)
 
-### Keep Null as it is (AutoIncrementing), 'name' add name for calling, 'url' add url by copying said url.
+###### Keep Null as it is (AutoIncrementing), 'name' add name for calling, 'url' add url by copying said url.
 
 #query = "INSERT INTO web_command VALUES (null, 'youtube', 'https://www.youtube.com/')"
 #cursor.execute(query)
 #conn.commit()
 
-# Creating a table with the desired columns
-cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+############################## Creating a table with the desired columns [CONTACTS] ##############################
+
+#cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+### Specifying column indices you want to import (0-based index)
+### Example: Importing the 1st and 3rd columns
+# desired_columns_indices = [0, 18]
+
+############ Reading the data from the CSV fole and inserting into SQLite table for desired columns ############
+
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute('''INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# ### Committing the changes
+# conn.commit()
+# conn.close()
+
+##########################################  ADDING A SINGLE CONTACT ##########################################
+
+# query = "INSERT INTO contacts VALUES (null, 'Arnav', '9373697593', null)"
+# cursor.execute(query)
+# conn.commit()
+
+##########################################  SEARCH CONTACT IN DATABASE ##########################################
+
+# query = 'Arnav'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(query.upper() + "\'S NUMBER IS: " + results[0][0])
--- engine/helper.py
+++ engine/helper.py
@@ -7,3 +7,13 @@ def extract_yt_term(command):
     match = re.search(pattern, command, re.IGNORECASE)
     # If a match is found, return the extracted song name, ELSE retun none
     return match.group(1) if match else None
+
+def remove_words(input_string, words_to_remove):
+    # Split the string into words
+    words = input_string.split()
+    # Remove unwated words from the list
+    filtered_words = [words for words in words if words.lower() not in words_to_remove]
+    # Join the filtered words back into a string
+    result_string = ' '.join(filtered_words)
+
+    return result_string
